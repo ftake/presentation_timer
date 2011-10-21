698518fdf5a235c3245f424b1af8ca901a1d2cfc
Fuminobu TAKEYAMA <f_takeyama@csg.is.titech.ac.jp>
2011-10-21 21:13:58 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+function formatTimeToString(min, sec) {
+	if (min < 10) {
+		min = "0" + min;
+	}
+	if (sec < 10) {
+		sec = "0" + sec;
+	}
+	return min + ":" + sec
+}
+
+function PresenTimer() {
+	this.passed = 0; //sec
+	this.timeout = 300; //sec
+	this.tick = function() {
+		this.passed++;
+		this.update();
+	}
+	this.update = function() {
+		var remaining = this.timeout - this.passed;
+		var min = Math.floor(remaining / 60);
+		var sec = remaining % 60;
+		fieldTime.firstChild.nodeValue = formatTimeToString(min, sec);
+	}
+	this.start = function() {
+		this.tod = setInterval("timer.tick()", 1000);
+		btnStartStop.setAttribute("onclick", "timer.stop()");
+		btnStartStop.setAttribute("value", "Stop");
+	}
+	this.reset = function() {
+		this.passed = 0;
+		this.update();
+	}
+	this.stop = function() {
+		clearInterval(this.tod);
+		this.tod = null;
+		btnStartStop.setAttribute("onclick", "timer.start()");
+		btnStartStop.setAttribute("value", "Start");
+	}
+}
+
+var btnReset = document.getElementById("btn_reset");
+btnReset.setAttribute("onclick", "timer.reset()");
+var btnConfig = document.getElementById("btn_config");
+btnConfig.setAttribute("onclick", "config()");
+var btnStartStop = document.getElementById("btn_start-stop");
+btnStartStop.setAttribute("onclick", "timer.start()");
+var btnSet = document.getElementById("btn_set");
+btnSet.setAttribute("onclick", "set()");
+var fieldTime = document.getElementById("time");
+
+var timer = new PresenTimer();
+
